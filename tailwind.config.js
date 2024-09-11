2255531da99d4a64b026e990a0630ea9522e961f
luis-keny <lucerobalvinluiskeny@gmail.com>
2024-09-10 21:17:58 -0500

    style: tailwind configuration and addition of space-grotesk and onest fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,43 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      "blue-1": "#f3f6fc",
+      "blue-2": "#e7eef7",
+      "blue-3": "#80ed99",
+      "blue-4": "#45dfb1",
+      "blue-5": "#0ad1c8",
+      "blue-6": "#14919b",
+      "blue-7": "#0b6477",
+      "blue-8": "#213a57",
+      "blue-9": "#16263b"
+    },
+    fontFamily: {
+      "space-grotesk": ["Space Grotesk", "sans-serif"],
+      "onest": ["Onest", "sans-serif"]
+    },
+    extend: {
+      fontSize: {
+        "1zp": "0.5rem",
+        "2zp": "0.625rem",
+        "3zp": "1rem",
+        "4zp": "1.125rem",
+        "5zp": "1.25rem",
+        "6zp": "1.5rem",
+        "7zp": "1.75rem",
+        "8zp": "2rem",
+        "9zp": "2.25rem",
+        "10zp": "2.5rem",
+        "11zp": "3rem",
+        "12zp": "4rem"
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    function ({ addVariant }) {
+      addVariant('child', '& > *');
+      addVariant('child-hover', '& > *:hover');
+    }
+  ],
 }
 
