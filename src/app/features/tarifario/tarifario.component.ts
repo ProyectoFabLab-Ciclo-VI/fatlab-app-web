74e99a29666d0049fe573ce9821cb945c1823e80
luis-keny <lucerobalvinluiskeny@gmail.com>
2024-11-05 20:50:05 -0500

    create tarifario.component

---
+++ src/app/features/tarifario/tarifario.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tarifario',
+  templateUrl: './tarifario.component.html',
+  styleUrl: './tarifario.component.css',
+  standalone: true,
+  imports: [],
+})
+export class TarifarioComponent {
+
+}
