91418414da6da712fe1ed37f47998963a647a917
luis-keny <lucerobalvinluiskeny@gmail.com>
2024-11-06 07:25:01 -0500

    remove gestion-insumo

--- src/app/features/gestion-insumo/gestion-insumo.module.ts
+++ src/app/features/gestion-insumo/gestion-insumo.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { GestionInsumoRoutingModule } from './gestion-insumo-routing.module';
-import { GestionInsumoComponent } from './gestion-insumo.component';
 
 
 @NgModule({
   declarations: [
-    GestionInsumoComponent
   ],
   imports: [
     CommonModule,
