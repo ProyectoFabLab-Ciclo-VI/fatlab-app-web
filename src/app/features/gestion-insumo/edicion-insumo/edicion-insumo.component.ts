9a008990e88e1abf67324c58ae3d8e20ec474cf2
luis-keny <lucerobalvinluiskeny@gmail.com>
2024-11-14 08:28:22 -0500

    chore(edicion insumo): change the button type and add component custom-select

--- src/app/features/gestion-insumo/edicion-insumo/edicion-insumo.component.ts
+++ src/app/features/gestion-insumo/edicion-insumo/edicion-insumo.component.ts
@@ -2,21 +2,43 @@ import { Component } from '@angular/core';
 
 import { FloatLabelComponent } from '../../../shared/components/float-label/float-label.component';
 import { CustomButtonComponent } from '../../../shared/components/custom-button/custom-button.component';
+import { CustomSelectComponent } from '../../../shared/components/custom-select/custom-select.component';
 
 import { InsumoService } from '../../../core/index.service.http';
 import { NotificationService } from '../../../core/index.service.trigger';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { Insumo } from '../../../core/index.data.model';
+import { SelectItem } from '../../../core/index.model.system';
 
 @Component({
   selector: 'app-edicion-insumo',
   templateUrl: './edicion-insumo.component.html',
   styleUrl: './edicion-insumo.component.css',
   standalone: true,
-  imports: [FloatLabelComponent, CustomButtonComponent, ReactiveFormsModule],
+  imports: [FloatLabelComponent, CustomButtonComponent, ReactiveFormsModule, CustomSelectComponent],
 })
 export class EdicionInsumoComponent {
   insumoForm: FormGroup = this.initializateFormGroupInsumo();
+
+  unidadesDeMedida: SelectItem[] = [
+    { value: 'g', viewValue: 'Gramos' },
+    { value: 'ml', viewValue: 'Mililitros' },
+    { value: 'mm', viewValue: 'milimetros'},
+  ];
+
+  categorias: SelectItem[] = [
+    { value: 'categoria-1', viewValue: 'Categoria 1' },
+    { value: 'categoria-2', viewValue: 'Categoria 2' },
+    { value: 'categoria-3', viewValue: 'Categoria 3' },
+  ];
+
+  marcas: SelectItem[] = [
+    { value: 'fill-red', viewValue: 'Filamento rojo' },
+    { value: 'fill-blue', viewValue: 'Filamento azul' },
+    { value: 'fill-black', viewValue: 'Filamento negro' },
+  ];
+
+  unidadMedidaSeleccionada: SelectItem = this.unidadesDeMedida[0];
 
   constructor(
     private insumoSrv: InsumoService,
