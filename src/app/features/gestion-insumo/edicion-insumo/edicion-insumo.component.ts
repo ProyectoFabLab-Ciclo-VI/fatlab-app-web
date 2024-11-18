a44cb4894ef8946de16226dc4fcda3a286763d9c
luis-keny <lucerobalvinluiskeny@gmail.com>
2024-11-18 03:30:01 -0500

    chore: edicion insumo with backend

--- src/app/features/gestion-insumo/edicion-insumo/edicion-insumo.component.ts
+++ src/app/features/gestion-insumo/edicion-insumo/edicion-insumo.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 import { CustomButtonComponent } from '../../../shared/components/custom-button/custom-button.component';
 import { CustomSelectComponent } from '../../../shared/components/custom-select/custom-select.component';
@@ -8,15 +8,19 @@ import { NotificationService } from '../../../core/index.service.trigger';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { Insumo } from '../../../core/index.data.model';
 import { SelectItem } from '../../../core/index.model.system';
+import { InventarioService } from '../../../core/service/https/inventario.service';
+import { Subscription } from 'rxjs';
+import { LoaderComponent } from '../../../shared/components/loader/loader.component';
 
 @Component({
   selector: 'app-edicion-insumo',
   templateUrl: './edicion-insumo.component.html',
   styleUrl: './edicion-insumo.component.css',
   standalone: true,
-  imports: [CustomButtonComponent, ReactiveFormsModule, CustomSelectComponent],
+  imports: [CustomButtonComponent, ReactiveFormsModule, CustomSelectComponent, LoaderComponent],
 })
-export class EdicionInsumoComponent {
+export class EdicionInsumoComponent implements OnInit, OnDestroy {
+  isSend: boolean = false;
   insumoForm: FormGroup = this.initializateFormGroupInsumo();
 
   unidadesDeMedida: SelectItem[] = [
@@ -25,11 +29,9 @@ export class EdicionInsumoComponent {
     { value: 'mm', viewValue: 'milimetros'},
   ];
 
-  categorias: SelectItem[] = [
-    { value: 'categoria-1', viewValue: 'Categoria 1' },
-    { value: 'categoria-2', viewValue: 'Categoria 2' },
-    { value: 'categoria-3', viewValue: 'Categoria 3' },
-  ];
+  categoriaInsumoSub: Subscription = new Subscription();
+  categorias: SelectItem[] = [];
+  categoriaSeleccionada!: SelectItem;
 
   marcas: SelectItem[] = [
     { value: 'fill-red', viewValue: 'Filamento rojo' },
@@ -42,35 +44,64 @@ export class EdicionInsumoComponent {
   constructor(
     private insumoSrv: InsumoService,
     private notificationSrv: NotificationService,
+    private inventarioSrv: InventarioService,
   ) { }
+
+  ngOnInit(): void {
+    this.inventarioSrv.getAllCategoriaInsumo().subscribe({
+      next: (categoriasInsumo) => {
+        for(let categoria of categoriasInsumo) {
+          const { id, nombre } = categoria;
+          this.categorias.push({ value: id, viewValue: nombre });
+        }
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    if(this.categoriaInsumoSub) this.categoriaInsumoSub.unsubscribe();
+  }
 
   private initializateFormGroupInsumo() {
     return new FormGroup({
       nombre: new FormControl(''),
       descripcion: new FormControl(''),
       unidadMedida: new FormControl(''),
+      cantidadTotal: new FormControl(0),
+      costeInsumo: new FormControl(0),
+      precioUnitario: new FormControl(0),
     });
   }
 
   public onSumit() {
-    const { nombre, descripcion, unidadMedida } = this.insumoForm.value;
+    const { nombre, descripcion, unidadMedida, cantidadTotal, costeInsumo, precioUnitario } = this.insumoForm.value;
     const insumo: Insumo = {
       id: 0,
       nombre: nombre,
       descripcion: descripcion,
       unidadMedida: unidadMedida,
+      cantidadTotal: cantidadTotal,
+      costeInsumo: costeInsumo,
+      marca: '',
+      precioUnitario: precioUnitario,
       activo: false,
+      categoriaInsumo: {
+        id: this.categoriaSeleccionada.value,
+        nombre: ''
+      },
     };
-
+    this.isSend = true;
     this.notificationSrv.addNotification('info', 'Guardando insumo...')
 
     this.insumoSrv.saveInsumo(insumo).subscribe({
       next: () => {
         this.notificationSrv.addNotification('success', 'Insumo guardado exitosamente');
+        this.isSend = false;
         this.insumoForm = this.initializateFormGroupInsumo();
       },
       error: (err) => {
         this.notificationSrv.addNotification('error', 'Error del servidor')
+        this.isSend = false;
         console.error(err);
       }
     });
