65da9a6c854fcf0cc5e6e7ad2f49673acc6739a5
luis-keny <lucerobalvinluiskeny@gmail.com>
2024-11-25 10:07:46 -0500

    refactor: export of missing index core functionalities

--- src/app/core/index.data.mapper.ts
+++ src/app/core/index.data.mapper.ts
@@ -2,3 +2,5 @@ export * from './data/mappers/maquina.mappers';
 export * from './data/mappers/insumo.mappers';
 export * from './data/mappers/categoriaInsumoMapper';
 export * from './data/mappers/configuracionCargo.mapper';
+export * from './data/mappers/categoriaMaquina.mappers';
+export * from './data/mappers/estadoMaquina.mappers';
--- src/app/core/index.service.http.ts
+++ src/app/core/index.service.http.ts
@@ -1,3 +1,5 @@
 export * from './service/https/auth-user.service';
 export * from './service/https/maquina.service';
 export * from './service/https/insumo.service';
+export * from './service/https/configuracion.service';
+export * from './service/https/inventario.service';
